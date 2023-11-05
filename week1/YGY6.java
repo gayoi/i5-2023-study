4ea25657a0f61e31234973a54ec8b6ee31fe66c5
gayoi <117894300+gayoi@users.noreply.github.com>
2023-11-06 04:55:57 +0900

    YGY6- 소수만들기

---
+++ week1/YGY6.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int[] nums) {
+        int answer = -1;
+        
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++){
+                for (int k = j + 1; k < nums.length; k++){
+                    int sum = num[i]+num[j]+num[k];
+                    
+                }
+                
+            }
+            
+        }
+
+        return answer;
+    }
+}
